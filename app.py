bb267cd15197130e35ab217c845915e1daf1abd7
unknown <btukenalieva@gmail.com>
2021-06-19 21:22:28 +0600

    add adding

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask.wrappers import Request
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    f = open('goods.txt', 'r', encoding='utf-8')
+    goods = f.readlines()
+    return render_template('index.html', goods = goods)
+
+@app.route('/add/', methods=['POST'])
+def add():
+    good = request.form['good']
+    f = open('goods.txt', 'a+', encoding='utf-8')
+    f.write(good + '\n')
+    f.close()
+    return """
+        <h2>Инвентарь пополнен</h2>
+        <a href='/'>Домой</a>
+        
+        """
